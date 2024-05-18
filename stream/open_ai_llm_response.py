d46880b59bd121a47faca49aebb0660b30bdba3e
asifrahaman13 <asifrahaman162@gmail.com>
2024-05-18 10:51:16 +0530

    wip: Made text to speech a class based component

--- stream/open_ai_llm_response.py
+++ stream/open_ai_llm_response.py
@@ -1,16 +1,14 @@
-import openai
 import os
 from openai import OpenAI
-import os
 from dotenv import load_dotenv
 import time
 
-
+# Load the environment variables
 load_dotenv()
 
-# Set your OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Create an OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+assert client, "OpenAI client is not set"
 
 
 class ChatResponse:
@@ -19,31 +17,38 @@ class ChatResponse:
         pass
 
     def chat_response(self, _query):
+
         # Record the start time
         start_time = time.time()
+
         # Create a completion
         stream = client.chat.completions.create(
             model="gpt-4o",
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a helpful assistant. help to make the text in points. a. b. etc should be the ordered list bullet points",
+                    "content": "You are a helpful assistant. help to make the text in points. a. b. etc should be the ordered list bullet points.",
                 },
                 {"role": "user", "content": _query},
             ],
             stream=True,
             max_tokens=100,
+            temperature=0.7,
         )
-
-        # Get the response from the completion
-        # response = stream.choices[0].message.content
-
-        # print(response)
-
+        
+        # Initialize a buffer to store the sentence
         sentence_buffer = ""
+
+        # Iterate over the stream of chunks
         for chunk in stream:
+
+            # Check if the completion is a message
             if chunk.choices[0].delta.content is not None:
+
+                # Append the chunk to the buffer
                 sentence_buffer += chunk.choices[0].delta.content
+
+                # Check if the sentence is complete
                 if sentence_buffer.endswith(('.', '!', '?')):
                      # Record the end time
                     end_time = time.time()
@@ -52,13 +57,10 @@ class ChatResponse:
                     elapsed_time = end_time - start_time
 
                     print(f"Elapsed time for open ai : {elapsed_time} seconds")
-
+                    
+                    # Yield the sentence
                     yield sentence_buffer.strip()
                     sentence_buffer = ""
-
-       
-        # return response
-
 
 if __name__ == "__main__":
     chat_response=ChatResponse()
--- stream/stream.py
+++ stream/stream.py
@@ -1,20 +1,31 @@
 import asyncio
 import websockets
-from deepgream_text_to_speech import synthesize_audio, chunk_text_by_sentence
+from deepgream_text_to_speech import TextToSpeech 
 from open_ai_llm_response import ChatResponse
 import time
 
 
 async def echo(websocket, path):
     messages_received = []
+
+    # Iterate over the messages received from the client
     async for message in websocket:
+
+        # Append the message to the list
         messages_received.append(message)
+
+        # Check if the list has only one message
         if len(messages_received) == 1:
             # Record the start time
             start_time = time.time()
-            compiled_text = " ".join(messages_received)
 
+            # Compile the messages into a single text
+            compiled_text = " ".join(messages_received)
+            
+            # Create a chat response object
             chat_response=ChatResponse()
+
+            # Get the chat response
             _chat_response = chat_response.chat_response(compiled_text)
 
             # chunks = chunk_text_by_sentence(_chat_response)
@@ -26,17 +37,22 @@ async def echo(websocket, path):
                     print(sentences)
                     
                     print("sent")
-                    audio = synthesize_audio(sentences)
+
+                    # Create a text to speech object
+                    text_to_speech = TextToSpeech()
+                    audio = text_to_speech.synthesize_audio(sentences)
                             # Record the end time
                     end_time = time.time()
                             # Calculate the elapsed time
                     elapsed_time = end_time - start_time
 
                     print(f"Elapsed time till receiving audio: {elapsed_time} seconds")
+
+                    # Send the audio to the client
                     await websocket.send(audio)
 
                     end_time = time.time()
-                            # Calculate the elapsed time
+                    # Calculate the elapsed time
                     elapsed_time = end_time - start_time
 
                     print(f"Elapsed time till after receiving audio: {elapsed_time} seconds")
@@ -46,8 +62,12 @@ async def echo(websocket, path):
 
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    try:
+        async with websockets.serve(echo, "localhost", 8765):
+            print("Server started")
+            await asyncio.Future()  # run forever
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
